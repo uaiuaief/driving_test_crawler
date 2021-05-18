fdf053099893346f13f0eb2b7af04f0d0564e4de
Jonathan <john@DESKTOP-HCRVJU7.localdomain>
2021-05-17 22:11:12 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+
+#This example requires Selenium WebDriver 3.13 or newer
+#driver = webdriver.Firefox(executable_path = '/home/john/DrivingTest/geckodriver') 
+
+firefox_options = Options()
+firefox_options.add_argument('--headless')
+
+with webdriver.Firefox(options=firefox_options) as driver:
+    wait = WebDriverWait(driver, 10)
+    driver.get("https://www.google.com")
+    driver.find_element(By.NAME, "q").send_keys("cheese" + Keys.RETURN)
+    first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "h3>div")))
+    print(first_result.get_attribute("textContent"))
