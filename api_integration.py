e3cbe58529b34f31d7a9beec948d2d98d23f300a
Jonathan <john@DESKTOP-HCRVJU7.localdomain>
2021-06-22 22:19:53 -0300

    add credentials to api integration requests

--- api_integration.py
+++ api_integration.py
@@ -1,8 +1,11 @@
+import datetime
 import requests
 from pprint import pprint
-from config import logger
+from config import logger, CRAWLER_USERNAME, CRAWLER_PASSWORD
 
 URL = 'http://localhost:8000/api'
+CREDENTIALS = (CRAWLER_USERNAME, CRAWLER_PASSWORD)
+
 
 def validate_customer_info(customer_pk):
     _set_customer_info(customer_pk, 'valid')
@@ -14,30 +17,68 @@ def _set_customer_info(customer_pk, value):
     endpoint = f"set-user-info-validation"
     full_url = f"{URL}/{endpoint}/"
     logger.debug(f'fetching: {full_url}')
-    r = requests.post(full_url, json={
+    r = requests.post(
+            full_url, 
+            auth=CREDENTIALS,
+            json={
         'user_id': customer_pk,
         'info_validation': value
         })
+
     r.raise_for_status()
 
 def ban_proxy(ip):
     endpoint = f"ban-proxy"
     full_url = f"{URL}/{endpoint}/"
     logger.debug(f'fetching: {full_url}')
-    r = requests.post(full_url, json={
-        'ip': ip,
-        })
+    r = requests.post(
+            full_url, 
+            auth=CREDENTIALS,
+            json={
+                'ip': ip,
+            })
+
     r.raise_for_status()
 
 def fetch_next_crawl():
     endpoint = f"proxy-customer-pair"
     full_url = f"{URL}/{endpoint}/"
     logger.debug(f'fetching: {full_url}')
-    r = requests.get(full_url)
-    #r.raise_for_status()
+    r = requests.get(full_url, auth=CREDENTIALS)
+    r.raise_for_status()
 
     if r.status_code == 200:
         return r.json()
     else: 
         return None
+
+def send_test_found_email(data):
+    endpoint = f"test-found"
+    full_url = f"{URL}/{endpoint}/"
+    logger.debug(f'fetching: {full_url}')
+
+    r = requests.post(
+            full_url, 
+            auth=CREDENTIALS,
+            json={
+                'user_id': data['user_id'],
+                'test_time': data['test_time'],
+                'test_date': data['test_date'],
+                'test_center_id': data['test_center_id'],
+            })
+    pprint(r.json())
+    r.raise_for_status()
+
+    if r.status_code == 200:
+        return r.json()
+    else: 
+        return None
+
+if __name__ == "__main__":
+    data = {
+            'user_id': 1,
+            'test_time': format(datetime.datetime.now(), "%H:%M"),
+            'test_date': format(datetime.datetime.now(), "%d-%m-%y"),
+            'test_center_id': 2,
+            }
 
--- config.py
+++ config.py
@@ -13,3 +13,6 @@ logger.addHandler(logging.StreamHandler())
 TWOCAPTCHA_API_KEY = os.environ.get('TWOCAPTCHA_API_KEY')
 DRIVING_LICENCE_NUMBER = os.environ.get('DRIVING_LICENCE_NUMBER')
 TEST_REF = os.environ.get('TEST_REF')
+
+CRAWLER_USERNAME = os.environ.get('CRAWLER_USERNAME')
+CRAWLER_PASSWORD = os.environ.get('CRAWLER_PASSWORD')
